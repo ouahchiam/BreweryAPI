9e6e5a0cb1a3b2d61e2cf3bba69abae3082938d7
Mohamed Ouahchia <Ouahchia.M@etoncollege.org.uk>
2024-05-24 10:02:53 +0100

    Initial Commit

---
+++ BreweryAPI/BreweryAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreweryAPIApp.swift
+//  BreweryAPI
+//
+//  Created by Ouahchia, Mohamed (JD) on 24/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct BreweryAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
